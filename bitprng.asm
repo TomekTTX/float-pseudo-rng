ee38aca945dca647ea5dba8e403436a187c2651f
TomekTTX <tomekttx@gmail.com>
2021-04-28 20:48:20 +0200

    Replaced tabs by spaces

--- bitprng.asm
+++ bitprng.asm
@@ -3,65 +3,65 @@
 
 .data
 
-base	equ	1397861645
-x		dd	1397861645
+base    equ 1397861645
+x       dd  1397861645
 
 .code
 
 _rnd PROC
-	push	ebp
-	mov		ebp, esp
-	sub		esp, 4
-	
-	finit
-	xor		edx, edx
-	mov		ecx, 5
-	mov		eax, x
+    push    ebp
+    mov     ebp, esp
+    sub     esp, 4
+    
+    finit
+    xor     edx, edx
+    mov     ecx, 5
+    mov     eax, x
 
-	call	xor_shift
-	
-	mov		x, eax
+    call    xor_shift
+    
+    mov     x, eax
 
-	and		eax, 007FFFFFh
-	or		eax, 3F800000h
-	mov		[esp], eax
-	fld		dword ptr [esp]
-	fld1
-	fsub
+    and     eax, 007FFFFFh
+    or      eax, 3F800000h
+    mov     [esp], eax
+    fld     dword ptr [esp]
+    fld1
+    fsub
 
-	add		esp, 4
-	pop		ebp
-	ret
+    add     esp, 4
+    pop     ebp
+    ret
 _rnd ENDP
 
 _seed PROC
-	push	ebp
-	mov		ebp, esp
+    push    ebp
+    mov     ebp, esp
 
-	mov		eax, [ebp + 8]
-	mov		ecx, 25
-	call	xor_shift
-	mov		x, eax
-	
-	mov		eax, [ebp + 8]
+    mov     eax, [ebp + 8]
+    mov     ecx, 25
+    call    xor_shift
+    mov     x, eax
+    
+    mov     eax, [ebp + 8]
 
-	pop		ebp
-	ret
+    pop     ebp
+    ret
 _seed ENDP
 
 xor_shift PROC
 shft:
-	bt		eax, 7
-	setc	dl
-	bt		eax, 14
-	setc	dh
-	xor		dl, dh
-	shr		dl, 1
-	rcl		eax, 1
-	
-	loop	shft
-	
-	ret
+    bt      eax, 7
+    setc    dl
+    bt      eax, 14
+    setc    dh
+    xor     dl, dh
+    shr     dl, 1
+    rcl     eax, 1
+    
+    loop    shft
+    
+    ret
 xor_shift ENDP
 
 END
